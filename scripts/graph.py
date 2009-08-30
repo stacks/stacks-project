ef2845bfc848d7da66e15e5dd4f379328b6de3b9
Aise Johan de Jong -- Hornet <aise.johan.de.jong@gmail.com>
2009-08-29 20:41:23 -0400

    Graph: improved graph creation script
    	modified:   scripts/graph.py

--- scripts/graph.py
+++ scripts/graph.py
@@ -10,24 +10,31 @@ path = get_path()
 
 labels = []
 
+lijstje = list_text_files(path)
+# Just pick one input file:
+#lijstje = ['sites']
 
-#lijstje = list_text_files(path)
-# file gets too big using all tex files. Just pick one:
-lijstje = ['schemes']
-
-# Check the tags file for correctness
+# All tags
 tags = get_tags(path)
+
+# Dictionary labels --> tags
 label_tags = dict((tags[n][1], tags[n][0]) for n in range(0, len(tags)))
 
-print "digraph dependencies {"
-print
-print "edge [dir=back]"
-print
+# Dictionary tags --> height in graph
+tags_nr = dict()
 n = 0
 while n < len(tags):
-	print "a" + tags[n][0] + "a",
-	print "[label=\"" + tags[n][0] + "\", color=green, fontcolor=blue, shape=rectangle]"
+	tags_nr[tags[n][0]] = 0
 	n = n + 1
+tags_nr['ZZZZ'] = 0
+
+# Dictionary tags --> referenced tags
+tags_refs = dict()
+n = 0
+while n < len(tags):
+	tags_refs[tags[n][0]] = []
+	n = n + 1
+tags_refs['ZZZZ'] = []
 
 ext = ".tex"
 for name in lijstje:
@@ -71,15 +78,22 @@ for name in lijstje:
 			if not proof_tag == 'ZZZZ':
 				refs = find_refs(line, name)
 				refs_proof.extend(refs)
-			if end_of_proof(line):
-				refs_proof = list(set(refs_proof))
+			if end_of_proof(line) and not proof_tag == 'ZZZZ':
+				refs_proof_set = set(refs_proof)
+				refs_proof_set.discard('ZZZZ')
+				refs_proof = list(refs_proof_set)
+				nr = 0
+				tags_proof = []
 				n = 0
 				while n < len(refs_proof):
 					ref_tag = find_tag(refs_proof[n], label_tags)
-					print "a" + proof_tag + "a",
-					print "->",
-					print "a" + ref_tag + "a"
+					tags_proof = tags_proof + [ref_tag]
+					nr_ref = tags_nr[ref_tag]
+					if nr_ref > nr:
+						nr = nr_ref
 					n = n + 1
+				tags_nr[proof_tag] = nr + 1
+				tags_refs[proof_tag] = tags_proof
 				refs_proof = []
 				in_proof = 0
 		else:
@@ -87,4 +101,132 @@ for name in lijstje:
 
 	tex_file.close()
 
-print "}"
+
+def print_whole_graph():
+	print "digraph dependencies {"
+	print
+	print "edge [dir=back]"
+	print
+	n = 0
+	while n < len(tags):
+		print "a" + tags[n][0] + "a",
+		print "[label=\"" + tags[n][0] + "\", color=green, fontcolor=blue, shape=rectangle]"
+		n = n + 1
+	n = 0
+	while n < len(tags):
+		m = 0
+		while m < len(tags_refs[tags[n][0]]):
+			print "a" + tags[n][0] + "a",
+			print "->",
+			print "a" + tags_refs[tags[n][0]][m] + "a"
+			m = m + 1
+		n = n + 1
+	print "}"
+
+def print_max_nodes():
+	T = 0
+	n = 0
+	while n < len(tags):
+		if tags_nr[tags[n][0]] > T:
+			T = tags_nr[tags[n][0]]
+		n = n + 1
+	print "Maximum height is",
+	print T,
+	print "attained by: "
+	n = 0
+	while n < len(tags):
+		if tags_nr[tags[n][0]] == T:
+			print tags[n][0],
+		n = n + 1
+	print
+	print "The next few maxima are: "
+	m = 1
+	while m < 20:
+		print "Height",
+		print T - m,
+		print " : ",
+		n = 0
+		while n < len(tags):
+			if tags_nr[tags[n][0]] == T - m:
+				print tags[n][0],
+			n = n + 1
+		print
+		m = m + 1
+def print_distribution():
+	T = 0
+	n = 0
+	while n < len(tags):
+		if tags_nr[tags[n][0]] > T:
+			T = tags_nr[tags[n][0]]
+		n = n + 1
+	print "Maximum height is",
+	print T,
+	print ". The distribution: "
+	m = T
+	while m >= 0:
+		print "Height",
+		print m,
+		print " : ",
+		n = 0
+		nr = 0
+		while n < len(tags):
+			if tags_nr[tags[n][0]] == m:
+				nr = nr + 1
+			n = n + 1
+		print nr
+		m = m - 1
+
+def print_tree_tag(mytag):
+	n = tags_nr[mytag]
+	mylist = [mytag]
+	oldset = set()
+	while n >= 2 and len(mylist) > 0:
+		print mylist
+		newlist = []
+		m = 0
+		while m < len(mylist):
+			newlist = newlist + tags_refs[mylist[m]]
+			m = m + 1
+		newset = set(newlist)
+		newset -= oldset
+		oldset |= newset
+		mylist = list(newset)
+		n = n - 1
+
+def print_graph_tag(mytag):
+	print "digraph dependencies {"
+	print
+	print "nodesep=0.05"
+	print "edge [dir=back]"
+	print
+	n = tags_nr[mytag]
+	mylist = [mytag]
+	oldset = set()
+	while n >= 0 and len(mylist) > 0:
+		m = 0
+		while m < len(mylist):
+			print "a" + mylist[m] + "a",
+			print "[label=\"" + mylist[m] + "\", color=green, fontcolor=blue, shape=rectangle, fixedsize=true, width=0.5, height=0.25]"
+			m = m + 1
+		newlist = []
+		m = 0
+		while m < len(mylist):
+			newlist = newlist + tags_refs[mylist[m]]
+			k = 0
+			while k < len(tags_refs[mylist[m]]):
+				print "a" + mylist[m] + "a",
+				print "->",
+				print "a" + tags_refs[mylist[m]][k] + "a"
+				k = k + 1
+			m = m + 1
+		newset = set(newlist)
+		newset -= oldset
+		oldset |= newset
+		mylist = list(newset)
+		n = n - 1
+	print "}"
+
+#print_whole_graph()
+#print_graph_tag('0200')
+print_max_nodes()
+#print_distribution()
