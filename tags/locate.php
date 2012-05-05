3d370c0f49db5183d8144084d72c7a8851ca0b17
Aise Johan de Jong <aise.johan.de.jong@gmail.com>
2012-05-05 17:37:05 -0400

    Check formatting strings in locate.php

--- tags/locate.php
+++ tags/locate.php
@@ -12,18 +12,20 @@
 <?php
 $TAG=strtoupper($_GET["tag"]);
 echo "<div style=\"margin-left: auto; margin-right: auto; text-align: left; width: 600px\">\n";
-if (file_exists("code/$TAG")) {
+if ((strlen($TAG) <> 4) or (ctype_alnum($TAG) == FALSE)) {
+	echo "Malformed input!<br>\n";
+	echo "Tags are 4 character strings of ";
+	echo "digits and letters.<br>\n";
+} elseif (file_exists("code/$TAG")) {
 	include("code/$TAG");
 } else {
 	echo "Tag $TAG does not exist.<br>\n";
 	echo "This may be because you mistyped it.<br>\n";
-	echo "Tags are 4 character strings of ";
-	echo "digits and letters.<br>";
-	echo "For more on tags click ";
-	echo "<a href=\"tags.html\">here</a>. ";
-	echo "To try again click ";
-	echo "<a href=\"query.php\">here</a>.";
 }
+echo "For more on tags click ";
+echo "<a href=\"tags.html\">here</a>. ";
+echo "To try again click ";
+echo "<a href=\"query.php\">here</a>.\n";
 echo "</div>";
 ?>
 
