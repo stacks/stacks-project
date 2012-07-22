e4a932f78b0c3c4a61a1619203c391dcf0350162
Aise Johan de Jong <aise.johan.de.jong@gmail.com>
2012-07-22 18:29:41 -0400

    Alter 'make tags' target
    With the new website setup we are compiling the live versions of
    the chapters and book in place. We change this by placing a
    version of scripts/latex.sh in tags/ and using this to compile
    the chapters and book.
    
    An added benefit of this is that the script allows us to use
    parallel make for the make tags target. This was previously
    pointed out on the blog here:
    
    http://math.columbia.edu/~dejong/wordpress/?p=651

---
+++ tags/latex.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Latex command to use
+LATEX=$1
+
+# Stem of latex file
+STEM=$2
+
+# Current directory
+OLD=${PWD}
+
+# Temporary directory
+TMPD=`mktemp -d --tmpdir=../../tmp`
+
+# Symbolically link or copy files to temp dir
+# 	Common for both cases
+ln -s $OLD/$STEM.bbl $OLD/hyperref.cfg $OLD/$STEM.tex $TMPD
+cp $STEM.toc $TMPD
+
+# 	Different
+if [ "$STEM" == "book" ]; then
+	ln -s $OLD/stacks-project-book.cls $TMPD;
+	cp book.aux $TMPD
+else
+	ln -s $OLD/preamble.tex $OLD/chapters.tex \
+		$OLD/stacks-project.cls $TMPD;
+	cp *.aux $TMPD
+fi
+
+# Latex the file in temporary directory
+# Exit without copying the results if there is a latex error.
+cd $TMPD
+$LATEX $STEM.tex
+if [ ! $? == 0 ]; then
+	rm -rf $TMPD;
+	exit 1;
+fi
+
+# Move newly created files back to stacks project directoy
+mv $STEM.pdf $STEM.dvi $STEM.aux $STEM.toc $OLD
+
+# Remove temporary directory
+cd $OLD
+rm -rf $TMPD
