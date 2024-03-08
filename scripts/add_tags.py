97a649a7a00c504b0b040ce97e70152cce474799
Stephen Gaito <stephen@perceptisys.co.uk>
2024-03-08 13:00:31 +0000

    corrected over zealous addition of trailing right parenthesis

--- scripts/add_tags.py
+++ scripts/add_tags.py
@@ -10,7 +10,7 @@ print("About to write",)
 print(len(new_tags),)
 print("new tags. Here is the list:")
 print()
-print_new_tags(new_tags))
+print_new_tags(new_tags)
 print()
 var = raw_input("Do you want to do this? (no/yes)")
 if var == "yes":
